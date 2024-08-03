7af41c1ab343abb5a86599df3a082262a41de88b
amanshridev <mailtoamanshri@proton.me>
2024-08-03 21:26:58 +0530

    second if else script

---
+++ 06_ifelse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+############
+#Author: Aman
+#Date: 03/08/2024
+#
+#My Simple if else script
+#
+#Version: V2
+#
+###########
+
+a=74
+b=20
+
+if [ a + b === 100 ]
+then 
+        echo "this script is working"
+
+
+else
+        echo "this is not working"
+
+fi
