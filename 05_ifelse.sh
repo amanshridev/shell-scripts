ddd8f260b9544db3bd921f3d1dabc28277e7500f
amanshridev <mailtoamanshri@proton.me>
2024-08-03 18:19:31 +0530

    first ifelse script

---
+++ 05_ifelse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#
+###############
+#Author: Aman
+#Date: 03/08.2024
+#
+#this is a if else script
+#
+#Version: V1
+#
+##############
+#
+a=4
+b=10
+if [ $a > $b ]
+then
+        echo "A is gretar"
+else
+        echo "B is greater"
+fi
