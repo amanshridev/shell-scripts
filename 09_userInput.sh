5f878d365275a229f10956c48a93293ccad46b0a
amanshridev <mailtoamanshri@proton.me>
2024-08-07 18:04:37 +0530

    taking user input script

---
+++ 09_userInput.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+##############
+#Author: Aman
+#Date: 07/08/2024
+#
+#Taking User in shell
+#
+#Version: V1
+#
+##############
+#
+
+read -p "What is your name ? " name
+
+echo "Araay is ${name}"
